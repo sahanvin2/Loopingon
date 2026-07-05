19c5fb4d572711e3e1664cec6f926dba3404dd74
Sahan Nawarathne <sahannawarathne2004@gmail.com>
2026-07-05 16:14:49 +0530

    Force replace favicon in all paths

---
+++ start_docker_droplet.js
@@ -0,0 +1,55 @@
+const { NodeSSH } = require('node-ssh');
+const ssh = new NodeSSH();
+
+async function run() {
+  try {
+    console.log('Connecting to DigitalOcean droplet...');
+    await ssh.connect({
+      host: '159.65.227.217',
+      username: 'root',
+      password: '@20040301Sa',
+      tryKeyboard: true,
+    });
+    console.log('Connected successfully!');
+
+    console.log('Building server image...');
+    await ssh.execCommand('docker build -t registry.digitalocean.com/loopingon/server:latest -f apps/server/Dockerfile .', { 
+      cwd: '/opt/loopingon',
+      onStdout: (c) => process.stdout.write(c.toString('utf8')),
+      onStderr: (c) => process.stderr.write(c.toString('utf8'))
+    });
+
+    console.log('Building web image...');
+    await ssh.execCommand('docker build -t registry.digitalocean.com/loopingon/web:latest -f apps/web/Dockerfile .', { 
+      cwd: '/opt/loopingon',
+      onStdout: (c) => process.stdout.write(c.toString('utf8')),
+      onStderr: (c) => process.stderr.write(c.toString('utf8'))
+    });
+
+    console.log('Building nginx image...');
+    await ssh.execCommand('docker build -t registry.digitalocean.com/loopingon/nginx:latest -f docker/nginx/Dockerfile .', { 
+      cwd: '/opt/loopingon',
+      onStdout: (c) => process.stdout.write(c.toString('utf8')),
+      onStderr: (c) => process.stderr.write(c.toString('utf8'))
+    });
+
+    console.log('Starting Docker Compose...');
+    const result = await ssh.execCommand('docker compose -f docker/docker-compose.prod.yml up -d', { 
+      cwd: '/opt/loopingon',
+      onStdout: (c) => process.stdout.write(c.toString('utf8')),
+      onStderr: (c) => process.stderr.write(c.toString('utf8'))
+    });
+    
+    if (result.code !== 0) {
+      console.error('Docker compose failed with exit code:', result.code);
+    } else {
+      console.log('\nDocker compose complete! Your site should be up.');
+    }
+    ssh.dispose();
+  } catch (err) {
+    console.error('Error:', err);
+    ssh.dispose();
+  }
+}
+
+run();
